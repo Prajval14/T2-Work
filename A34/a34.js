193a29f7868c9ae9840fe3e47e932dabd7c4bc1f
Prajval Patel <prajval1420@gmail.com>
2024-02-11 17:22:26 -0500

    final code

--- A34/a34.js
+++ A34/a34.js
@@ -1,48 +1,42 @@
-let form_input = document.getElementById('myForm');
-var input1 = document.getElementById('usd');
-var input2 = document.getElementById('cad');
-let usd_input = 0;
-let cad_input = 0;
+// Made By - Prajval Patel C0911611
+// Date - Feb 11, 2024 
+
+// Declaring variables
+let input1 = document.getElementById('usd');
+let input2 = document.getElementById('cad');
 let conversion_rate = 1.35;
 
+//Function on form submit
 document.getElementById("myForm").addEventListener("submit", function(event) {
     event.preventDefault();
 
-    usd_input = form_input.elements['usd'].value;
-    cad_input = form_input.elements['cad'].value;
-    
-    input1.value = convertToUSD(cad_input);
-    input2.value = convertToCAD(usd_input); 
+    //Setting output in form elements
+    if(input1.value != '') {
+        input2.value = convertToCAD(input1.value);     
+    }
+    else {
+        input1.value = convertToUSD(input2.value);
+    }        
 });
 
-function disableAlternative() {    
-    if (input1.value.trim() !== '') {
-        input2.disabled = true;
-    } else {
-        input2.disabled = false;
-    }
-    
-    if (input2.value.trim() !== '') {
-        input1.disabled = true;
-    } else {
-        input1.disabled = false;
-    }
-}
+//USD value on input function
+document.getElementById('usd').addEventListener('input', function() {
+    document.getElementById('cad').value = '';
+});
 
+//CAD value on on input function
+document.getElementById('cad').addEventListener('input', function() {
+    document.getElementById('usd').value = '';
+});
+
+//Function to convert cad to usd
 function convertToUSD(cad_value) {
     converted_USD = cad_value / conversion_rate;
     return parseFloat(converted_USD.toFixed(3));
 }
 
+//Function to convert usd to cad
 function convertToCAD(usd_value) {
     converted_CAD = conversion_rate * usd_value;
     return parseFloat(converted_CAD.toFixed(3));
-}
-
-function submitFunction() {
-    usd_input = form_input.elements['usd'].value;
-    cad_input = form_input.elements['cad'].value;
-    
-    input1.value = convertToUSD(cad_input);
-    input2.value = convertToCAD(usd_input);    
 }
